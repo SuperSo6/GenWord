9eb12259f6952d7433a73fb083788e1011e3a1b2
SuperSo6 <romain.gas@icloud.com>
2022-11-13 17:44:09 +0100

    Update TreeWords.h

--- Projet_C_Gen_Word/TreeWords.h
+++ Projet_C_Gen_Word/TreeWords.h
@@ -5,6 +5,33 @@
 #ifndef PROJET_C_GEN_WORD_TREEWORDS_H
 #define PROJET_C_GEN_WORD_TREEWORDS_H
 
+typedef struct noeud_lettre *p_noeud_lettre;
+
+struct Flechi_cellule{
+    
+};
+
+
+typedef struct cellule{
+
+    p_noeud_lettre enfant;
+    struct cellule * suivant;
+
+};
+
+
+typedef struct s_std_liste{
+    p_cell head;
+}t_std_liste;
+
+
+struct noeud_lettre{
+    char lettre;
+    t_std_liste * enfants;
+    F_std_liste * FormeFlechie;
+
+};
+
 
 
 #endif //PROJET_C_GEN_WORD_TREEWORDS_H
